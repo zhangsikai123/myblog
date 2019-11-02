aa7f7709cb8e1da4a36f0d53bde284b981e18fe2
sikai.zhang <sikai.zhang@17zuoye.com>
2019-11-02 23:16:03 +0800

    ci server listen 80

--- ci_server.py
+++ ci_server.py
@@ -18,5 +18,5 @@ def after_git_push():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8001)),
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 80)),
             debug=app.config['DEBUG'])
